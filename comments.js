a18fd45153f2b4b601660434659632e6f2252cb2
Hossein Poorhemati <45974093+Hosseinpoorhemati@users.noreply.github.com>
2023-10-03 00:03:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,78 @@
-// create web server with express
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-// import cors
-const cors = require('cors');
-// import mongoose
-const mongoose = require('mongoose');
-// import body-parser
-const bodyParser = require('body-parser');
-// import routes
-const comments = require('./routes/comments');
-// import database
-const db = require('./config/database');
-// import error handler
-const errorHandler = require('./middleware/errorHandler');
-
-// connect to database
-mongoose.connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
-
-// set up cors
-app.use(cors());
-
-// set up body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
+// create web server
+// use express framework
+var express = require('express');
+var app = express();
+// use body parser middleware to get data from POST request
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
-// set up routes
-app.use('/comments', comments);
-
-// set up error handler
-app.use(errorHandler);
-
-// start server
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+// use express-session middleware to manage session
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+app.use(session({
+    secret: 'asdasdasdasdasd',
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore()
+}));
+// use mysql module
+var mysql = require('mysql');
+// create mysql connection pool
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'test'
+});
+// use passport module
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+app.use(passport.initialize());
+app.use(passport.session());
+// create passport strategy
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        pool.query('select * from user where username=?', [username], function (err, results) {
+            if (err) {
+                done(err);
+            } else {
+                if (results.length === 0) {
+                    done(null, false);
+                } else {
+                    var user = results[0];
+                    if (username === user.username && password === user.password) {
+                        done(null, user);
+                    } else {
+                        done(null, false);
+                    }
+                }
+            }
+        });
+    }
+));
+// serialize and deserialize user
+passport.serializeUser(function (user, done) {
+    done(null, user.username);
+});
+passport.deserializeUser(function (username, done) {
+    pool.query('select * from user where username=?', [username], function (err, results) {
+        if (err) {
+            done(err);
+        } else {
+            var user = results[0];
+            done(null, user);
+        }
+    });
+});
+// setup static directory
+app.use(express.static('public'));
+// setup view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+// setup routes
+app.get('/', function (req, res) {
+    res.redirect('/comments');
+});
+app.get('/comments', function (req, res) {
+    pool.query('select * from comment order
